b6558686af3c36c686ac41817c4ea5609502a629
Araceli Yanez <adyst@users.noreply.github.com>
2019-08-06 17:17:07 -0700

    Adding ngrx-data tutorial.

---
+++ ngrx-data-lab/src/app/heroes/heroes/heroes.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { Hero } from '../../core';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  selected: Hero;
+  heroes$: Observable<Hero[]>;
+  loading$: Observable<boolean>;
+
+  constructor(private heroService: HeroService) {
+    this.heroes$ = heroService.entities$;
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  add(hero: Hero) {
+    this.heroService.add(hero);
+  }
+
+  close() {
+    this.selected = null;
+  }
+
+  delete(hero: Hero) {
+    this.heroService.delete(hero);
+    this.close();
+  }
+
+  enableAddMode() {
+    this.selected = <any>{};
+  }
+
+  getHeroes() {
+    this.heroService.getAll();
+    this.close();
+  }
+
+  select(hero: Hero) {
+    this.selected = hero;
+  }
+
+  update(hero: Hero) {
+    this.heroService.update(hero);
+  }
+}
