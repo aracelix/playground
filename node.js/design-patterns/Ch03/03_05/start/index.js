35e25ab0b03dab9aa0ddbef2938e6b91e6e30b46
Araceli Yanez <adyst@users.noreply.github.com>
2020-09-16 21:07:11 -0700

    Completed proxy, composite, and decorator structural patterns.

--- node.js/design-patterns/Ch03/03_05/start/index.js
+++ node.js/design-patterns/Ch03/03_05/start/index.js
@@ -1,10 +1,18 @@
 var CatalogItem = require('./CatalogItem');
+var CatalogGroup = require('./CatalogGroup');
 
 var boots = new CatalogItem("Leather Boots", 79.99);
 var sneakers = new CatalogItem("Kicks", 39.99);
 var flipFlops = new CatalogItem("California work boots", 19.99);
 
-console.log( 'boots total: ', `$${boots.total}` );
+var group_shoes = new CatalogGroup("Shoes and Such", [boots, sneakers, flipFlops]);
+var group_food = new CatalogGroup("Food for while you try on clothes", [
+  new CatalogItem("Milkshake", 5.99),
+  new CatalogItem("Fries", 3.99)
+]);
+var keychain = new CatalogItem("Key Chain", 0.99);
 
-boots.print();
-sneakers.print();
+var catalog = new CatalogGroup("Clothes and Food", [keychain, group_shoes, group_food]);
+
+console.log(`$${catalog.total}`);
+catalog.print();
--- node.js/design-patterns/Ch03/03_07/start/index.js
+++ node.js/design-patterns/Ch03/03_07/start/index.js
@@ -1,12 +1,23 @@
 var Shopper = require('./Shopper');
-var InventoryItem = require('./InventoryItem');
+var { 
+  InventoryItem,
+  GoldenInventoryItem,
+  DiamondInventoryItem
+}   = require('./InventoryItem');
 
-var alex = new Shopper('Alex', 100);
+var alex = new Shopper('Alex', 30000);
 
 var walkman = new InventoryItem("Walkman", 29.99);
 var necklace = new InventoryItem("Necklace", 9.99);
 
-alex.purchase(necklace);
-alex.purchase(walkman);
+var gold_necklace = new GoldenInventoryItem(necklace);
+var diamond_gold_necklace = new DiamondInventoryItem(gold_necklace);
+
+var diamond_walkman = new DiamondInventoryItem(walkman);
+
+alex.purchase(diamond_gold_necklace);
+alex.purchase(diamond_walkman);
 
 alex.printStatus();
+
+diamond_walkman.print();
