de762feccd5899aa048ecd05ffa4360fbe8445ac
Araceli Yanez <adyst@users.noreply.github.com>
2020-10-01 08:56:08 -0700

    Added controllers for each type.

---
+++ node.js/express-locallibrary-tutorial/controllers/authorController.js
@@ -0,0 +1,33 @@
+var Author = require('../models/author');
+
+exports.author_list = (req, res) => {
+  res.send('Not implemented');
+};
+
+exports.author_detail = (req, res) => {
+  res.send('Not implemented');
+};
+
+exports.author_create_get = (req, res) => {
+  res.send('Not implemented');
+};
+
+exports.author_create_post = (req, res) => {
+  res.send('Not implemented');
+};
+
+exports.author_delete_post = (req, res) => {
+  res.send("Not implemented");
+};
+
+exports.author_update_get = (req, res) => {
+  res.send("Not implemented");
+};
+
+exports.author_update_post = (req, res) => {
+  res.send("Not implemented");
+};
+
+exports.author_delete_get = (req, res) => {
+  res.send("NotImpelemented");
+}
